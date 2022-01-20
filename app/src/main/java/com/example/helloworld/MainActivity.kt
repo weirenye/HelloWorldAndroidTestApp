88c48efabd66aa6e5caf0f4bd6bd40a652782499
yeweirenye <weirenye@hotmail.com>
2022-01-20 16:31:07 +0800

    Initial commit

---
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.helloworld
+import android.util.Log
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        Log.d("Test", "This is printed")
+    }
+}
